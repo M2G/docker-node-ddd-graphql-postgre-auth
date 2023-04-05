b49fb03396022d15b7fc78039f29c5daf99f7583
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-04-06 00:55:46 +0200

    feat: update sql table users

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,6 +1,17 @@
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
-  username varchar(255),
-  password_hash varchar(255),
-  UNIQUE (username)
+  username VARCHAR(255),
+  -- https://stackoverflow.com/questions/386294/what-is-the-maximum-length-of-a-valid-email-address
+  email VARCHAR(254),
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  username VARCHAR(255),
+  password VARCHAR(255),
+  created_at INTEGER,
+  modified_at INTEGER,
+  reset_password_token VARCHAR(255),
+  reset_password_expires INTEGER DEFAULT 0 NOT NULL,
+  deleted_at INTEGER DEFAULT 0 NOT NULL,
+  last_connected_at INTEGER DEFAULT 0 NOT NULL,
+  UNIQUE (email)
 );
