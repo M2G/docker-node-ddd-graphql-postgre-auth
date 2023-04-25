2591374d28bf05c7180c49715072db9202f3af80
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-04-26 01:53:54 +0200

    fix: test unit authenticate

--- tests/unit/app/authenticate/post_authenticate.spec.ts
+++ tests/unit/app/authenticate/post_authenticate.spec.ts
@@ -1,20 +1,19 @@
-/*import { faker } from '@faker-js/faker';
+/*eslint-disable*/
+import { faker } from '@faker-js/faker';
 import postUsecase from  'src/app/authenticate/post';
 
 describe('App -> User -> Post', () => {
   const randomEmail = faker.internet.email();
-  const randomUserName = faker.internet.userName();
-  const randomPassword = faker.internet.password();
 
   let useCase: { authenticate: any; };
 
   describe('Success path', () => {
     beforeEach(() => {
-      const MockRepository = {
+      const MockRepository: any = {
         authenticate: (data: any) => data
       }
 
-      const MockRedis = {
+      const MockRedis: any  = {
         set: () => (data: any) => data
       }
 
@@ -27,8 +26,6 @@ describe('App -> User -> Post', () => {
     it('test', async () => {
      const body = {
         email: randomEmail,
-        username: randomUserName,
-        password: randomPassword,
       }
 
       const lists = await useCase.authenticate({ ...body })
@@ -40,25 +37,28 @@ describe('App -> User -> Post', () => {
   describe('Fail path', () => {
     const body = {
       email: randomEmail,
-      username: randomUserName,
-      password: randomPassword,
     }
 
     beforeEach(() => {
-      const MockRepository = {
+      const MockRepository: any = {
         authenticate: () => Promise.reject('Error')
       }
 
+      const MockRedis: any  = {
+        set: () => (data: any) => data
+      }
+
       useCase = postUsecase({
-        usersRepository: MockRepository
-      })
+        usersRepository: MockRepository,
+        redis: MockRedis
+      } as any)
     })
 
     it('should display error on rejection', async () => {
 
       let error: any;
       try {
-        await useCase.authenticate({ body })
+        await useCase.authenticate({ ...body })
       } catch (e: any) {
         error = e.message
       }
@@ -67,4 +67,4 @@ describe('App -> User -> Post', () => {
   })
 
 })
-*/
+
