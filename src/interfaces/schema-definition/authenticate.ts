2f4ab60affc9f56d21fd2a9e76c1e88d136efd55
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2024-04-06 04:24:36 +0200

    feat: code refactor

--- src/interfaces/schema-definition/authenticate.ts
+++ src/interfaces/schema-definition/authenticate.ts
@@ -3,6 +3,7 @@ import { join } from 'path';
 import gql from 'graphql-tag';
 import { comparePassword } from 'infra/encryption';
 import type IUser from 'core/IUser';
+import HttpException from 'infra/support';
 
 export default ({ postUseCase, jwt, logger }: any) => {
   const typeDefs = gql(readFileSync(join(__dirname, '../..', 'auth.graphql'), 'utf-8'));
@@ -11,39 +12,58 @@ export default ({ postUseCase, jwt, logger }: any) => {
     Mutation: {
       signin: async (parent: any, args: any) => {
         const { input } = args;
-        const { email, password } = input as IUser;
+        const { email, password } = (input as IUser) || {};
 
         console.log('input input input input', { email, password });
 
         try {
           const data: IUser = await postUseCase.authenticate({ email });
 
-          if (!data || !data.email) {
-            throw new Error(`User not found (email: ${data.email}).`);
+          console.log('authenticate authenticate authenticate', data);
+
+          if (!data?.email) {
+            logger.info(`User not found (email: ${email}).`);
+            throw new HttpException(401, 'USER_NOT_FOUND');
+            /*
+            throw new GraphQLError(`User not found (email: ${data.email}).`, {
+              extensions: {
+                code: Status.UNAUTHORIZED,
+                http: { status: 401 },
+              },
+            });
+             */
           }
 
           const match = comparePassword(password, data.password);
 
           if (!match) {
-            throw new Error('Wrong username and password combination.');
+            throw new HttpException(401, 'WRONG_COMBINATION');
+            /*
+               throw new GraphQLError('Wrong username and password combination. 222', {
+              extensions: {
+                code: 'SOMETHING_WRONG_CONBINATION',
+                http: {
+                  status: 401,
+                },
+              },
+            });
+             */
           }
 
           const payload: IUser = {
-            id: data.id,
             email: data.email,
+            id: data.id,
             password: data.password,
           };
 
           const options = {
             audience: [],
-            expiresIn: 1 * 60,
+            expiresIn: 5 * 60,
             subject: data.email,
           };
 
           // if user is found and password is right, create a token
           const token: string = jwt.signin(options)(payload);
-
-          console.log('token token token', token);
 
           logger.info({ token });
 
